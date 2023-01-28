2919052ff0c9d5a7bce5e6bc307fcbbbc92dbfc8
olawale afuye <walosha@yahoo.com>
2023-01-28 05:36:11 +0100

    Removed node_modules

--- node_modules/@bull-board/api/dist/src/handlers/emptyQueue.d.ts
+++
@@ -1,2 +0,0 @@
-import { BullBoardRequest, ControllerHandlerReturnType } from '../../typings/app';
-export declare const emptyQueueHandler: (req: BullBoardRequest) => Promise<ControllerHandlerReturnType>;
--- node_modules/@bull-board/api/dist/src/handlers/redisStats.d.ts
+++
@@ -1,2 +0,0 @@
-import { BullBoardRequest, ControllerHandlerReturnType } from '../../typings/app';
-export declare function redisStatsHandler({ queues: bullBoardQueues, }: BullBoardRequest): Promise<ControllerHandlerReturnType>;
