36707715c16119dae72c42656db94680a8230244
olawale afuye <walosha@yahoo.com>
2023-01-28 04:26:34 +0100

    initial commit, bootsrapped without background task

--- index.js
+++ index.js
@@ -15,16 +15,40 @@ app.use(
 );
 
 app.use(fileUpload());
+
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.render("form");
 });
 
+app.get("/result", (req, res) => {
+  const imgDirPath = path.join(__dirname, "./public/images");
+  let imgFiles = fs.readdirSync(imgDirPath).map((image) => {
+    return `images/${image}`;
+  });
+  res.render("result", { imgFiles });
+});
+
 app.post("/upload", async function (req, res) {
   const { image } = req.files;
 
   if (!image) return res.sendStatus(400);
+  const imageName = path.parse(image.name).name;
+  const processImage = (size) =>
+    sharp(image.data)
+      .resize(size, size)
+      .webp({ lossless: true })
+      .toFile(`./public/images/${imageName}-${size}.webp`);
+
+  sizes = [90, 96, 120, 144, 160, 180, 240, 288, 360, 480, 720, 1440];
+  Promise.all(sizes.map(processImage));
+  let counter = 0;
+  for (let i = 0; i < 10_000_000_000_000_000; i++) {
+    counter++;
+  }
+
+  res.redirect("/result");
 });
 
 app.listen(3000, function () {
